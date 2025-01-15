5d57768f81d4de864faf29c8d86d9fb7e8cb37d4
unerringCaprition <penumbruh@gmail.com>
2025-01-14 19:42:40 -0800

    yayyy digging :)

--- Game 2/player_tank.gd
+++ Game 2/player_tank.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@export var foreground: TileMapLayer
 
 const SPEED = 60.0
 const JUMP_VELOCITY = -150.0
@@ -8,6 +9,25 @@ const GRAVITY = Vector2(0, 400)
 var in_air_last_frame = false
 
 func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("secondary"):
+		var dig_position_1 = foreground.local_to_map(position)
+		var dig_position_2 = foreground.local_to_map(position)
+		
+		var horizontal := Input.get_axis("left", "right")
+		var vertical := Input.get_axis("up", "down")
+		
+		if horizontal:
+			dig_position_1.x += horizontal
+			if vertical == -1:
+				dig_position_2.y += vertical
+			elif vertical == 1:
+				dig_position_2.y += horizontal
+		if vertical:
+			dig_position_1.y += vertical
+		
+		foreground.erase_cell(dig_position_1)
+		foreground.erase_cell(dig_position_2)
+	
 	if position.y > 140:
 		$SpeakerDie.play()
 		position = Vector2(2.0, -3.0)
@@ -53,4 +73,4 @@ func _physics_process(delta: float) -> void:
 		var collision = get_slide_collision(i)
 		if collision.get_normal().x != 0 && !$AreaClimb.has_overlapping_bodies() && is_on_floor():
 			position += Vector2(-collision.get_normal().x * 0.1, -8)
-			print("beep!")
+			#print("beep!")
